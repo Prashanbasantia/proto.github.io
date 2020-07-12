746666224ad37dd19738e98fbd58754c69de8b78
Prashanbasantia <45598143+Prashanbasantia@users.noreply.github.com>
2020-07-12 12:59:17 +0530

    change
    basic changes

---
+++ js/custome.js
@@ -0,0 +1,86 @@
+ 
+
+ //////////////|||| All custome code will written here with specifeid comments||||||
+
+
+
+ //||category-item to show category-content
+ function openCity(evt, cityName) {
+  var i, category_content, category_item;
+  category_content = document.getElementsByClassName("category-content");
+  for (i = 0; i < category_content.length; i++) {
+    category_content[i].style.display = "none";
+  }
+  category_item = document.getElementsByClassName("category-item");
+  for (i = 0; i < category_item.length; i++) {
+    category_item[i].className = category_item[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+//||end category-item to show category-content
+
+// || owl-carousel js code 
+$('.owl-carousel.category').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: [
+      "<i class='fas fa-caret-left'></i>",
+      "<i class='fas fa-caret-right'></i>"
+    ],
+    dots: false, 
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+//||end owl-carousel js code
+
+
+// type write 
+
+function typeEffect(element, speed) {
+  var text = element.innerHTML;
+  element.innerHTML = "";
+  
+  var i = 0;
+  var timer = setInterval(function() {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+
+
+// application
+var speed = 75;
+var h1 = document.querySelector('h1');
+var p = document.querySelector('p');
+var delay = h1.innerHTML.length * speed + speed;
+
+// type affect to header
+typeEffect(h1, speed);
+
+
+// type affect to body
+setTimeout(function(){
+  p.style.display = "inline-block";
+  typeEffect(p, speed);
+}, delay);
+//Typing effect intro by @sarangpidadi
+
+//end wripe writer
