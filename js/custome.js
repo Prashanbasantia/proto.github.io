4539d89ed571b78156b3a8184eaf8e10ef37dc00
Prashanbasantia <45598143+Prashanbasantia@users.noreply.github.com>
2020-07-21 16:14:17 +0530

    product page
    product page

--- js/custome.js
+++ js/custome.js
@@ -48,39 +48,6 @@ $('.owl-carousel.category').owlCarousel({
 //||end owl-carousel js code
 
 
-// type write 
 
-function typeEffect(element, speed) {
-  var text = element.innerHTML;
-  element.innerHTML = "";
-  
-  var i = 0;
-  var timer = setInterval(function() {
-    if (i < text.length) {
-      element.append(text.charAt(i));
-      i++;
-    } else {
-      clearInterval(timer);
-    }
-  }, speed);
-}
+// for parallax-img
 
-
-// application
-var speed = 75;
-var h1 = document.querySelector('h1');
-var p = document.querySelector('p');
-var delay = h1.innerHTML.length * speed + speed;
-
-// type affect to header
-typeEffect(h1, speed);
-
-
-// type affect to body
-setTimeout(function(){
-  p.style.display = "inline-block";
-  typeEffect(p, speed);
-}, delay);
-//Typing effect intro by @sarangpidadi
-
-//end wripe writer
